5982a4db5e4e23f1ff50f98e16465e7b514bff57
vreddy <vipinreddy27@gmail.com>
2025-10-30 14:14:16 +0000

    fix build

--- terraform/application/locals.tf
+++ terraform/application/locals.tf
@@ -23,6 +23,4 @@ locals {
   key_vault_name = "${var.azure_resource_prefix}-${var.service_short}-${var.config_short}-app-kv"
   
   resource_group_name = "${var.azure_resource_prefix}-${var.service_short}-${var.config_short}-rg"
-
-  postgres_ssl_mode = local.environment == "production" ? "require" : "prefer"
 }
