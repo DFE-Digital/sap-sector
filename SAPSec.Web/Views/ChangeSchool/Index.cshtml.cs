bf750f6df98d9536b56e6c9eef8c982df50b4f8e
vreddy <vipinreddy27@gmail.com>
2025-11-12 22:58:35 +0000

    add navigation

---
+++ SAPSec.Web/Views/ChangeSchool/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SAPSec.Web.Views.ChangeSchool
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
