8d503bdf43e97952ce1733e964ae7c545b89feaf
Hardik Desai <hardik.desai@education.gov.uk>
2025-12-01 14:02:18 +0000

    added tests for home page and layout

---
+++ Tests/SAPSec.Integration.Tests/HomeControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using FluentAssertions;
+using SAPSec.Integration.Tests.Infrastructure;
+using SAPSec.Web.Constants;
+
+namespace SAPSec.Integration.Tests;
+
+public class HomeControllerTests(WebApplicationSetupFixture fixture) : IClassFixture<WebApplicationSetupFixture>
+{
+    [Fact]
+    public async Task HomePage_ReturnsHtmlWithCorrectTitle()
+    {
+        var response = await fixture.Client.GetAsync("/");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        response.Content.Headers.ContentType?.MediaType.Should().Be("text/html");
+
+        var content = await response.Content.ReadAsStringAsync();
+        content.Should().Contain($"{PageTitles.ServiceHome} - {LayoutConstants.ServiceName} - GOV.UK");
+    }
+
+    [Fact]
+    public async Task HomePage_HasFooterLinks()
+    {
+        var response = await fixture.Client.GetAsync("/");
+        var content = await response.Content.ReadAsStringAsync();
+
+        content.Should().Contain("<a href=\"/cookies\">Cookies</a>");
+        content.Should().Contain("<a href=\"/accessibility\">Accessibility</a>");
+        content.Should().Contain("<a href=\"/terms-of-use\">Terms of use</a>");
+        content.Should().Contain("https://www.gov.uk/government/publications/privacy-information-education-providers-workforce-including-teachers/privacy-information-education-providers-workforce-including-teachers");
+    }
+
+    [Fact]
+    public async Task HomePage_HasSkipLink()
+    {
+        var response = await fixture.Client.GetAsync("/");
+        var content = await response.Content.ReadAsStringAsync();
+
+        content.Should().Contain("<a href=\"#main-content\" class=\"govuk-skip-link\"");
+    }
+
+    [Fact]
+    public async Task HomePage_HasCrownCopyright()
+    {
+        var response = await fixture.Client.GetAsync("/");
+        var content = await response.Content.ReadAsStringAsync();
+
+        content.Should().Contain("© Crown copyright");
+        content.Should().Contain("Open Government Licence v3.0");
+    }
+}
---
+++ Tests/SAPSec.Web.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using SAPSec.Web.Constants;
+using SAPSec.Web.Controllers;
+
+namespace SAPSec.Web.Tests.Controllers;
+
+public class HomeControllerTests
+{
+    private readonly HomeController _controller = new(null!, null!);
+
+    [Fact]
+    public void Index_Get_ReturnsViewResult()
+    {
+        var result = _controller.Index();
+
+        result.Should().NotBeNull();
+        result.Should().BeOfType<ViewResult>();
+    }
+
+    [Fact]
+    public void Index_Get_SetsPageTitleInViewData()
+    {
+        var result = _controller.Index() as ViewResult;
+
+        result.Should().NotBeNull();
+        result.ViewData.ContainsKey(ViewDataKeys.Title).Should().BeTrue();
+        result.ViewData[ViewDataKeys.Title].Should().Be(PageTitles.ServiceHome);
+    }
+
+    [Fact]
+    public void Error_Get_ReturnsViewResult()
+    {
+        var result = _controller.Error();
+
+        result.Should().NotBeNull();
+        result.Should().BeOfType<ViewResult>();
+    }
+}
