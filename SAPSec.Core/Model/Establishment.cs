806caf431f6703e882e0e3bc23480678fd27fecd
Dan Murfitt <Dan.MURFITT@EDUCATION.GOV.UK>
2025-12-03 06:46:40 +0000

    Add Data Model to Solution

---
+++ SAPSec.Core/Model/Establishment.cs
@@ -0,0 +1,46 @@
+﻿using SAPSec.Core.Model.KS4.Absence;
+using SAPSec.Core.Model.KS4.Destinations;
+using SAPSec.Core.Model.KS4.Performance;
+using SAPSec.Core.Model.KS4.SubjectEntries;
+using SAPSec.Core.Model.KS4.Workforce;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAPSec.Core.Model
+{
+    [ExcludeFromCodeCoverage]
+    public class Establishment
+    {
+        public string URN { get; set; } = string.Empty;
+        public string LAId { get; set; } = string.Empty;
+
+        public EstablishmentMetadata Metadata { get; set; } = new();
+
+        public EstablishmentPerformance KS4Performance { get; set; } = new();
+        public LAPerformance LAPerformance { get; set; } = new();
+        public EnglandPerformance EnglandPerformance { get; set; } = new();
+
+        public EstablishmentSubjectEntries KS4SubjectEntries { get; set; } = new();
+        public LASubjectEntries LASubjectEntries { get; set; } = new();
+        public EnglandSubjectEntries EnglandSubjectEntries { get; set; } = new();
+
+        public EstablishmentDestinations EstablishmentDestinations { get; set; } = new();
+        public LADestinations LADestinations { get; set; } = new();
+        public EnglandDestinations EnglandDestinations { get; set; } = new();
+
+
+        public EstablishmentAbsence EstablishmentAbsence { get; set; } = new();
+        public LAAbsence LAAbsence { get; set; } = new();
+        public EnglandAbsence Absence { get; set; } = new(); // Will eventually need one per phase
+
+        public EstablishmentWorkforce Workforce { get; set; } = new(); // Will eventually need one per phase
+
+
+
+    }
+}
---
+++ SAPSec.Core/Model/KS4/Performance/EstablishmentPerformance.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAPSec.Core.Model.KS4.Performance
+{
+    [ExcludeFromCodeCoverage]
+    public class EstablishmentPerformance
+    {
+        public string Id { get; set; } = string.Empty;
+        /// 
+        ///Attainment 8 Boys filtered by Establishment for Current year 
+        /// 
+        public double? Attainment8_Boy_Est_Current_Num { get; set; }
+        public string Attainment8_Boy_Est_Current_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 Disadvantaged filtered by Establishment for Current year 
+        /// 
+        public double? Attainment8_Dis_Est_Current_Num { get; set; }
+        public string Attainment8_Dis_Est_Current_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 EAL - English as additional language filtered by Establishment for Current year 
+        /// 
+        public double? Attainment8_EAL_Est_Current_Num { get; set; }
+        public string Attainment8_EAL_Est_Current_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 Girls filtered by Establishment for Current year 
+        /// 
+        public double? Attainment8_Grl_Est_Current_Num { get; set; }
+        public string Attainment8_Grl_Est_Current_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 Not disadvantaged filtered by Establishment for Current year 
+        /// 
+        public double? Attainment8_NDi_Est_Current_Num { get; set; }
+        public string Attainment8_NDi_Est_Current_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 Non-Mobile filtered by Establishment for Current year 
+        /// 
+        public double? Attainment8_NMo_Est_Current_Num { get; set; }
+        public string Attainment8_NMo_Est_Current_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 Total filtered by Establishment for Current year 
+        /// 
+        public double? Attainment8_Tot_Est_Current_Num { get; set; }
+        public string Attainment8_Tot_Est_Current_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 Boys filtered by Establishment for Current year 
+        /// 
+        public double? EngMaths49_Boy_Est_Current_Pct { get; set; }
+        public string EngMaths49_Boy_Est_Current_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 Disadvantaged filtered by Establishment for Current year 
+        /// 
+        public double? EngMaths49_Dis_Est_Current_Pct { get; set; }
+        public string EngMaths49_Dis_Est_Current_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 EAL - English as additional language filtered by Establishment for Current year 
+        /// 
+        public double? EngMaths49_EAL_Est_Current_Pct { get; set; }
+        public string EngMaths49_EAL_Est_Current_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 Girls filtered by Establishment for Current year 
+        /// 
+        public double? EngMaths49_Grl_Est_Current_Pct { get; set; }
+        public string EngMaths49_Grl_Est_Current_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 Not disadvantaged filtered by Establishment for Current year 
+        /// 
+        public double? EngMaths49_NDi_Est_Current_Pct { get; set; }
+        public string EngMaths49_NDi_Est_Current_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 Non-Mobile filtered by Establishment for Current year 
+        /// 
+        public double? EngMaths49_NMo_Est_Current_Pct { get; set; }
+        public string EngMaths49_NMo_Est_Current_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 Total filtered by Establishment for Current year 
+        /// 
+        public double? EngMaths49_Tot_Est_Current_Pct { get; set; }
+        public string EngMaths49_Tot_Est_Current_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 Boys filtered by Establishment for Current year 
+        /// 
+        public double? EngMaths59_Boy_Est_Current_Pct { get; set; }
+        public string EngMaths59_Boy_Est_Current_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 Disadvantaged filtered by Establishment for Current year 
+        /// 
+        public double? EngMaths59_Dis_Est_Current_Pct { get; set; }
+        public string EngMaths59_Dis_Est_Current_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 EAL - English as additional language filtered by Establishment for Current year 
+        /// 
+        public double? EngMaths59_EAL_Est_Current_Pct { get; set; }
+        public string EngMaths59_EAL_Est_Current_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 Girls filtered by Establishment for Current year 
+        /// 
+        public double? EngMaths59_Grl_Est_Current_Pct { get; set; }
+        public string EngMaths59_Grl_Est_Current_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 Not disadvantaged filtered by Establishment for Current year 
+        /// 
+        public double? EngMaths59_NDi_Est_Current_Pct { get; set; }
+        public string EngMaths59_NDi_Est_Current_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 Non-Mobile filtered by Establishment for Current year 
+        /// 
+        public double? EngMaths59_NMo_Est_Current_Pct { get; set; }
+        public string EngMaths59_NMo_Est_Current_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 Total filtered by Establishment for Current year 
+        /// 
+        public double? EngMaths59_Tot_Est_Current_Pct { get; set; }
+        public string EngMaths59_Tot_Est_Current_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 Boys filtered by Establishment for Previous year 
+        /// 
+        public double? Attainment8_Boy_Est_Previous_Num { get; set; }
+        public string Attainment8_Boy_Est_Previous_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 Disadvantaged filtered by Establishment for Previous year 
+        /// 
+        public double? Attainment8_Dis_Est_Previous_Num { get; set; }
+        public string Attainment8_Dis_Est_Previous_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 EAL - English as additional language filtered by Establishment for Previous year 
+        /// 
+        public double? Attainment8_EAL_Est_Previous_Num { get; set; }
+        public string Attainment8_EAL_Est_Previous_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 Girls filtered by Establishment for Previous year 
+        /// 
+        public double? Attainment8_Grl_Est_Previous_Num { get; set; }
+        public string Attainment8_Grl_Est_Previous_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 Non-Mobile filtered by Establishment for Previous year 
+        /// 
+        public double? Attainment8_NMo_Est_Previous_Num { get; set; }
+        public string Attainment8_NMo_Est_Previous_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 Total filtered by Establishment for Previous year 
+        /// 
+        public double? Attainment8_Tot_Est_Previous_Num { get; set; }
+        public string Attainment8_Tot_Est_Previous_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 Boys filtered by Establishment for Previous year 
+        /// 
+        public double? EngMaths49_Boy_Est_Previous_Pct { get; set; }
+        public string EngMaths49_Boy_Est_Previous_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 Disadvantaged filtered by Establishment for Previous year 
+        /// 
+        public double? EngMaths49_Dis_Est_Previous_Pct { get; set; }
+        public string EngMaths49_Dis_Est_Previous_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 EAL - English as additional language filtered by Establishment for Previous year 
+        /// 
+        public double? EngMaths49_EAL_Est_Previous_Pct { get; set; }
+        public string EngMaths49_EAL_Est_Previous_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 Girls filtered by Establishment for Previous year 
+        /// 
+        public double? EngMaths49_Grl_Est_Previous_Pct { get; set; }
+        public string EngMaths49_Grl_Est_Previous_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 Non-Mobile filtered by Establishment for Previous year 
+        /// 
+        public double? EngMaths49_NMo_Est_Previous_Pct { get; set; }
+        public string EngMaths49_NMo_Est_Previous_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 Total filtered by Establishment for Previous year 
+        /// 
+        public double? EngMaths49_Tot_Est_Previous_Pct { get; set; }
+        public string EngMaths49_Tot_Est_Previous_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 Boys filtered by Establishment for Previous year 
+        /// 
+        public double? EngMaths59_Boy_Est_Previous_Pct { get; set; }
+        public string EngMaths59_Boy_Est_Previous_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 Disadvantaged filtered by Establishment for Previous year 
+        /// 
+        public double? EngMaths59_Dis_Est_Previous_Pct { get; set; }
+        public string EngMaths59_Dis_Est_Previous_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 EAL - English as additional language filtered by Establishment for Previous year 
+        /// 
+        public double? EngMaths59_EAL_Est_Previous_Pct { get; set; }
+        public string EngMaths59_EAL_Est_Previous_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 Girls filtered by Establishment for Previous year 
+        /// 
+        public double? EngMaths59_Grl_Est_Previous_Pct { get; set; }
+        public string EngMaths59_Grl_Est_Previous_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 Non-Mobile filtered by Establishment for Previous year 
+        /// 
+        public double? EngMaths59_NMo_Est_Previous_Pct { get; set; }
+        public string EngMaths59_NMo_Est_Previous_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 Total filtered by Establishment for Previous year 
+        /// 
+        public double? EngMaths59_Tot_Est_Previous_Pct { get; set; }
+        public string EngMaths59_Tot_Est_Previous_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 Boys filtered by Establishment for Previous2 year 
+        /// 
+        public double? Attainment8_Boy_Est_Previous2_Num { get; set; }
+        public string Attainment8_Boy_Est_Previous2_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 Disadvantaged filtered by Establishment for Previous2 year 
+        /// 
+        public double? Attainment8_Dis_Est_Previous2_Num { get; set; }
+        public string Attainment8_Dis_Est_Previous2_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 EAL - English as additional language filtered by Establishment for Previous2 year 
+        /// 
+        public double? Attainment8_EAL_Est_Previous2_Num { get; set; }
+        public string Attainment8_EAL_Est_Previous2_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 Girls filtered by Establishment for Previous2 year 
+        /// 
+        public double? Attainment8_Grl_Est_Previous2_Num { get; set; }
+        public string Attainment8_Grl_Est_Previous2_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 Not disadvantaged filtered by Establishment for Previous2 year 
+        /// 
+        public double? Attainment8_NDi_Est_Previous2_Num { get; set; }
+        public string Attainment8_NDi_Est_Previous2_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 Non-Mobile filtered by Establishment for Previous2 year 
+        /// 
+        public double? Attainment8_NMo_Est_Previous2_Num { get; set; }
+        public string Attainment8_NMo_Est_Previous2_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///Attainment 8 Total filtered by Establishment for Previous2 year 
+        /// 
+        public double? Attainment8_Tot_Est_Previous2_Num { get; set; }
+        public string Attainment8_Tot_Est_Previous2_Num_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 Boys filtered by Establishment for Previous2 year 
+        /// 
+        public double? EngMaths49_Boy_Est_Previous2_Pct { get; set; }
+        public string EngMaths49_Boy_Est_Previous2_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 Disadvantaged filtered by Establishment for Previous2 year 
+        /// 
+        public double? EngMaths49_Dis_Est_Previous2_Pct { get; set; }
+        public string EngMaths49_Dis_Est_Previous2_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 EAL - English as additional language filtered by Establishment for Previous2 year 
+        /// 
+        public double? EngMaths49_EAL_Est_Previous2_Pct { get; set; }
+        public string EngMaths49_EAL_Est_Previous2_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 Girls filtered by Establishment for Previous2 year 
+        /// 
+        public double? EngMaths49_Grl_Est_Previous2_Pct { get; set; }
+        public string EngMaths49_Grl_Est_Previous2_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 Mobile filtered by Establishment for Previous2 year 
+        /// 
+        public double? EngMaths49_Mob_Est_Previous2_Pct { get; set; }
+        public string EngMaths49_Mob_Est_Previous2_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 4 to 9 Total filtered by Establishment for Previous2 year 
+        /// 
+        public double? EngMaths49_Tot_Est_Previous2_Pct { get; set; }
+        public string EngMaths49_Tot_Est_Previous2_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 Boys filtered by Establishment for Previous2 year 
+        /// 
+        public double? EngMaths59_Boy_Est_Previous2_Pct { get; set; }
+        public string EngMaths59_Boy_Est_Previous2_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 Disadvantaged filtered by Establishment for Previous2 year 
+        /// 
+        public double? EngMaths59_Dis_Est_Previous2_Pct { get; set; }
+        public string EngMaths59_Dis_Est_Previous2_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 EAL - English as additional language filtered by Establishment for Previous2 year 
+        /// 
+        public double? EngMaths59_EAL_Est_Previous2_Pct { get; set; }
+        public string EngMaths59_EAL_Est_Previous2_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 Girls filtered by Establishment for Previous2 year 
+        /// 
+        public double? EngMaths59_Grl_Est_Previous2_Pct { get; set; }
+        public string EngMaths59_Grl_Est_Previous2_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 Mobile filtered by Establishment for Previous2 year 
+        /// 
+        public double? EngMaths59_Mob_Est_Previous2_Pct { get; set; }
+        public string EngMaths59_Mob_Est_Previous2_Pct_Reason { get; set; } = string.Empty;
+
+        /// 
+        ///English and Maths grades 5 to 9 Total filtered by Establishment for Previous2 year 
+        /// 
+        public double? EngMaths59_Tot_Est_Previous2_Pct { get; set; }
+        public string EngMaths59_Tot_Est_Previous2_Pct_Reason { get; set; } = string.Empty;
+
+
+
+
+    }
+}
