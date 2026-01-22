bee4cf35137b0075e36667e1fdd8ac1adfc0f2dc
vreddy <vipinreddy27@gmail.com>
2026-01-22 12:08:05 +0000

    add school details

--- SAPSec.Web/Controllers/SchoolController.cs
+++ SAPSec.Web/Controllers/SchoolController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SAPSec.Core.Interfaces.Services;
+using SAPSec.Core.Services;
 using SAPSec.Web.Constants;
+using SAPSec.Web.ViewModels;
 
 namespace SAPSec.Web.Controllers;
 
@@ -36,4 +38,107 @@ public class SchoolController : Controller
         ViewData[ViewDataKeys.BreadcrumbNode] = BreadcrumbNodes.SchoolHome(urn);
         return View(school);
     }
+
+    [HttpGet]
+    [Route("school-details")]
+    public IActionResult SchoolDetails(string urn)
+    {
+        var school = _schoolDetailsService.TryGetByUrn(urn);
+        if (school != null)
+        {
+            ViewData[ViewDataKeys.BreadcrumbNode] = BreadcrumbNodes.SchoolHome(urn);
+            return View(school);
+        }
+        else
+        {
+            _logger.LogInformation($"{urn} was not found on School Controller");
+            return RedirectToAction("Error");
+        }
+    }
+
+    [HttpGet]
+    [Route("what-is-a-similar-school")]
+    public IActionResult WhatIsASimilarSchool(string urn)
+    {
+        var school = _schoolDetailsService.TryGetByUrn(urn);
+        if (school != null)
+        {
+            ViewData[ViewDataKeys.BreadcrumbNode] = BreadcrumbNodes.SchoolHome(urn);
+            return View(school);
+        }
+        else
+        {
+            _logger.LogInformation($"{urn} was not found on School Controller");
+            return RedirectToAction("Error");
+        }
+    }
+
+    [HttpGet]
+    [Route("view-similar-schools")]
+    public IActionResult ViewSimilarSchools(string urn)
+    {
+        var school = _schoolDetailsService.TryGetByUrn(urn);
+        if (school != null)
+        {
+            ViewData[ViewDataKeys.BreadcrumbNode] = BreadcrumbNodes.SchoolHome(urn);
+            return View(school);
+        }
+        else
+        {
+            _logger.LogInformation($"{urn} was not found on School Controller");
+            return RedirectToAction("Error");
+        }
+    }
+
+    [HttpGet]
+    [Route("attendance")]
+    public IActionResult Attendance(string urn)
+    {
+        var school = _schoolDetailsService.TryGetByUrn(urn);
+        if (school != null)
+        {
+            ViewData[ViewDataKeys.BreadcrumbNode] = BreadcrumbNodes.SchoolHome(urn);
+            return View(school);
+        }
+        else
+        {
+            _logger.LogInformation($"{urn} was not found on School Controller");
+            return RedirectToAction("Error");
+        }
+    }
+
+    [HttpGet]
+    [Route("ks4-headline-measures")]
+    public IActionResult Ks4HeadlineMeasures(string urn)
+    {
+        var school = _schoolDetailsService.TryGetByUrn(urn);
+        if (school != null)
+        {
+            ViewData[ViewDataKeys.BreadcrumbNode] = BreadcrumbNodes.SchoolHome(urn);
+            return View(school);
+        }
+        else
+        {
+            _logger.LogInformation($"{urn} was not found on School Controller");
+            return RedirectToAction("Error");
+        }
+    }
+
+    [HttpGet]
+    [Route("ks4-core-subjects")]
+    public IActionResult Ks4CoreSubjects(string urn)
+    {
+        var school = _schoolDetailsService.TryGetByUrn(urn);
+        if (school != null)
+        {
+            ViewData[ViewDataKeys.BreadcrumbNode] = BreadcrumbNodes.SchoolHome(urn);
+            return View(school);
+        }
+        else
+        {
+            _logger.LogInformation($"{urn} was not found on School Controller");
+            return RedirectToAction("Error");
+        }
+    }
+
 }
