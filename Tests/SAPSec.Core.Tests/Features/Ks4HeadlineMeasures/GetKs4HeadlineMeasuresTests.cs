244df222b03b7d261ddc1e63edf997d860237cb8
vreddy <vipinreddy27@gmail.com>
2026-03-16 15:50:57 +0000

    added extra tests

--- Tests/SAPSec.Core.Tests/Features/Ks4HeadlineMeasures/GetKs4HeadlineMeasuresTests.cs
+++ Tests/SAPSec.Core.Tests/Features/Ks4HeadlineMeasures/GetKs4HeadlineMeasuresTests.cs
@@ -4,6 +4,7 @@ using SAPSec.Core.Features.Ks4HeadlineMeasures;
 using SAPSec.Core.Features.Ks4HeadlineMeasures.UseCases;
 using SAPSec.Core.Interfaces.Services;
 using SAPSec.Core.Model;
+using SAPSec.Core.Model.KS4.Destinations;
 using SAPSec.Core.Model.KS4.Performance;
 
 namespace SAPSec.Core.Tests.Features.Ks4HeadlineMeasures;
@@ -148,6 +149,92 @@ public class GetKs4HeadlineMeasuresTests
         result.EngMaths59YearByYear.School.Should().Be(new Ks4HeadlineMeasureSeries(null, null, null));
         result.EngMaths59YearByYear.LocalAuthority.Should().Be(new Ks4HeadlineMeasureSeries(null, null, null));
         result.EngMaths59YearByYear.England.Should().Be(new Ks4HeadlineMeasureSeries(null, null, null));
+        result.DestinationsThreeYearAverage.Should().Be(new Ks4HeadlineMeasureAverage(null, null, null));
+        result.DestinationsYearByYear.School.Should().Be(new Ks4HeadlineMeasureSeries(null, null, null));
+        result.DestinationsYearByYear.LocalAuthority.Should().Be(new Ks4HeadlineMeasureSeries(null, null, null));
+        result.DestinationsYearByYear.England.Should().Be(new Ks4HeadlineMeasureSeries(null, null, null));
+        result.DestinationsEducationThreeYearAverage.Should().Be(new Ks4HeadlineMeasureAverage(null, null, null));
+        result.DestinationsEducationYearByYear.School.Should().Be(new Ks4HeadlineMeasureSeries(null, null, null));
+        result.DestinationsEducationYearByYear.LocalAuthority.Should().Be(new Ks4HeadlineMeasureSeries(null, null, null));
+        result.DestinationsEducationYearByYear.England.Should().Be(new Ks4HeadlineMeasureSeries(null, null, null));
+        result.DestinationsEmploymentThreeYearAverage.Should().Be(new Ks4HeadlineMeasureAverage(null, null, null));
+        result.DestinationsEmploymentYearByYear.School.Should().Be(new Ks4HeadlineMeasureSeries(null, null, null));
+        result.DestinationsEmploymentYearByYear.LocalAuthority.Should().Be(new Ks4HeadlineMeasureSeries(null, null, null));
+        result.DestinationsEmploymentYearByYear.England.Should().Be(new Ks4HeadlineMeasureSeries(null, null, null));
+    }
+
+    [Fact]
+    public async Task Execute_WhenDestinationsDataExists_ReturnsDestinationMeasures()
+    {
+        var schoolDetailsServiceMock = new Mock<ISchoolDetailsService>();
+        var repositoryMock = new Mock<IKs4PerformanceRepository>();
+
+        schoolDetailsServiceMock
+            .Setup(x => x.TryGetByUrnAsync("123456"))
+            .ReturnsAsync(CreateSchoolDetails("123456"));
+
+        repositoryMock
+            .Setup(x => x.GetByUrnAsync("123456"))
+            .ReturnsAsync(new Ks4HeadlineMeasuresData(
+                null,
+                null,
+                null,
+                new EstablishmentDestinations
+                {
+                    AllDest_Tot_Est_Current_Pct = 94.4,
+                    AllDest_Tot_Est_Previous_Pct = 93.4,
+                    AllDest_Tot_Est_Previous2_Pct = 92.4,
+                    Education_Tot_Est_Current_Pct = 72.2,
+                    Education_Tot_Est_Previous_Pct = 71.2,
+                    Education_Tot_Est_Previous2_Pct = 70.2,
+                    Employment_Tot_Est_Current_Pct = 22.2,
+                    Employment_Tot_Est_Previous_Pct = 22.2,
+                    Employment_Tot_Est_Previous2_Pct = 22.2
+                },
+                new LADestinations
+                {
+                    AllDest_Tot_LA_Current_Pct = 91.0,
+                    AllDest_Tot_LA_Previous_Pct = 90.0,
+                    AllDest_Tot_LA_Previous2_Pct = 89.0,
+                    Education_Tot_LA_Current_Pct = 68.0,
+                    Education_Tot_LA_Previous_Pct = 67.0,
+                    Education_Tot_LA_Previous2_Pct = 66.0,
+                    Employment_Tot_LA_Current_Pct = 23.0,
+                    Employment_Tot_LA_Previous_Pct = 23.0,
+                    Employment_Tot_LA_Previous2_Pct = 23.0
+                },
+                new EnglandDestinations
+                {
+                    AllDest_Tot_Eng_Current_Pct = 88.5,
+                    AllDest_Tot_Eng_Previous_Pct = 87.5,
+                    AllDest_Tot_Eng_Previous2_Pct = 86.5,
+                    Education_Tot_Eng_Current_Pct = 64.5,
+                    Education_Tot_Eng_Previous_Pct = 63.5,
+                    Education_Tot_Eng_Previous2_Pct = 62.5,
+                    Employment_Tot_Eng_Current_Pct = 24.0,
+                    Employment_Tot_Eng_Previous_Pct = 24.5,
+                    Employment_Tot_Eng_Previous2_Pct = 25.0
+                }));
+
+        var sut = new GetKs4HeadlineMeasures(repositoryMock.Object, schoolDetailsServiceMock.Object);
+
+        var result = await sut.Execute(new GetKs4HeadlineMeasuresRequest("123456"));
+
+        result.Should().NotBeNull();
+        result.DestinationsThreeYearAverage.Should().Be(new Ks4HeadlineMeasureAverage(93.4m, 90.0m, 87.5m));
+        result.DestinationsYearByYear.School.Should().Be(new Ks4HeadlineMeasureSeries(94.4m, 93.4m, 92.4m));
+        result.DestinationsYearByYear.LocalAuthority.Should().Be(new Ks4HeadlineMeasureSeries(91.0m, 90.0m, 89.0m));
+        result.DestinationsYearByYear.England.Should().Be(new Ks4HeadlineMeasureSeries(88.5m, 87.5m, 86.5m));
+
+        result.DestinationsEducationThreeYearAverage.Should().Be(new Ks4HeadlineMeasureAverage(71.2m, 67.0m, 63.5m));
+        result.DestinationsEducationYearByYear.School.Should().Be(new Ks4HeadlineMeasureSeries(72.2m, 71.2m, 70.2m));
+        result.DestinationsEducationYearByYear.LocalAuthority.Should().Be(new Ks4HeadlineMeasureSeries(68.0m, 67.0m, 66.0m));
+        result.DestinationsEducationYearByYear.England.Should().Be(new Ks4HeadlineMeasureSeries(64.5m, 63.5m, 62.5m));
+
+        result.DestinationsEmploymentThreeYearAverage.Should().Be(new Ks4HeadlineMeasureAverage(22.2m, 23.0m, 24.5m));
+        result.DestinationsEmploymentYearByYear.School.Should().Be(new Ks4HeadlineMeasureSeries(22.2m, 22.2m, 22.2m));
+        result.DestinationsEmploymentYearByYear.LocalAuthority.Should().Be(new Ks4HeadlineMeasureSeries(23.0m, 23.0m, 23.0m));
+        result.DestinationsEmploymentYearByYear.England.Should().Be(new Ks4HeadlineMeasureSeries(24.0m, 24.5m, 25.0m));
     }
 
     [Fact]
