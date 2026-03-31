f87a723f229ce5bd7ec573597283f5c0e97ebd8b
mahmoodcli <mahmoodcli@SL645718>
2026-03-31 13:53:37 +0100

    Implement cookie settings page and top-banner confirmation flow

--- SAPSec.Web/ViewModels/Components/CookiesViewModel.cs
+++ SAPSec.Web/ViewModels/Components/CookiesViewModel.cs
@@ -1,6 +1,7 @@
 namespace SAPSec.Web.ViewModels.Components;
 
-public class CookiesViewModel(string cookieName)
+public class CookiesViewModel(string cookieName, string bannerState)
 {
     public string CookieName => cookieName;
+    public string BannerState => bannerState;
 }
