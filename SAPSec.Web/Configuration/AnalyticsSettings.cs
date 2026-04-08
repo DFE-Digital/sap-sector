be7f9715b884429d5197cd14966a16c9433c64de
mahmoodcli <mahmoodcli@SL645718>
2026-04-08 14:19:40 +0100

    Added AnalyticsSettings

---
+++ SAPSec.Web/Configuration/AnalyticsSettings.cs
@@ -0,0 +1,9 @@
+namespace SAPSec.Web.Configuration;
+
+public class AnalyticsSettings
+{
+    public string? GoogleTagManagerId { get; set; }
+    public string? GoogleTagManagerAuth { get; set; }
+    public string? GoogleTagManagerPreview { get; set; }
+    public string? ClarityId { get; set; }
+}
