6eb40f234cdcb53b59079c01f9342e8740eb5aaa
shaheislamdfe <shahe.islam@education.gov.uk>
2026-04-13 13:52:47 +0100

    Add DFE Analytics Module (#130)
    ### Overview
     
     - Adding the DFE Analytics workflow
     - GCP Auth scaffolding with project ID
     - Disabled by default, team can enable by setting the flag in tfvars
    
    ### Review steps
    
    Run `make test terraform-plan` after enabling dfe analytics within the
    test environment (Set enable_dfe_analytics_federated_auth to true) and
    validate plan output

--- terraform/application/locals.tf
+++ terraform/application/locals.tf
@@ -25,6 +25,19 @@ locals {
   dsi_urls = local.dsi_config[local.dsi_environment]
 
   key_vault_name = "${var.azure_resource_prefix}-${var.service_short}-${var.config_short}-app-kv"
-  
+
   resource_group_name = "${var.azure_resource_prefix}-${var.service_short}-${var.config_short}-rg"
+
+  dfe_analytics_enabled = var.enable_dfe_analytics_federated_auth
+
+  federated_auth_configmap = local.dfe_analytics_enabled ? {
+    DfeAnalytics__Environment = local.environment
+    DfeAnalytics__ProjectId   = module.dfe_analytics[0].bigquery_project_id
+    DfeAnalytics__DatasetId   = module.dfe_analytics[0].bigquery_dataset
+    DfeAnalytics__TableId     = module.dfe_analytics[0].bigquery_table_name
+  } : {}
+
+  federated_auth_secrets = local.dfe_analytics_enabled ? {
+    DfeAnalytics__CredentialsJson = module.dfe_analytics[0].google_cloud_credentials
+  } : {}
 }
