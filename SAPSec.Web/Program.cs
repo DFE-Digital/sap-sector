97d938da2245c1996994914029e55ab7f380b3f3
Christopher Morriss <Christopher.MORRISS@EDUCATION.GOV.UK>
2026-04-14 14:58:00 +0100

    updated conditional logic

--- SAPSec.Web/Program.cs
+++ SAPSec.Web/Program.cs
@@ -80,7 +80,7 @@ public class Program
             builder.Services.AddDsiAuthentication(builder.Configuration);
         }
 
-        if (!builder.Environment.IsEnvironment("Development") || !builder.Environment.IsEnvironment("IntegrationTests"))
+        if (builder.Environment.EnvironmentName is not "Development" || builder.Environment.EnvironmentName is not "IntegrationTests")
         {
             builder.Services.AddDfeAnalytics().AddAspNetCoreIntegration(options =>
             {
@@ -222,7 +222,7 @@ public class Program
         //    app.UseDfeAnalytics();
         //}
 
-        if (!builder.Environment.IsEnvironment("Development") || !builder.Environment.IsEnvironment("IntegrationTests"))
+        if (builder.Environment.EnvironmentName is not "Development" || builder.Environment.EnvironmentName is not "IntegrationTests")
         {
             app.UseDfeAnalytics();
         }
