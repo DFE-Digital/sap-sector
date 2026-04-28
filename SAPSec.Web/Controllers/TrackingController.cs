4636a8d9e112598dee5612e97ef1ba4bbefb7e87
Christopher Morriss <Christopher.MORRISS@EDUCATION.GOV.UK>
2026-04-28 13:13:16 +0100

    tidy tracking controller and view

--- SAPSec.Web/Controllers/TrackingController.cs
+++ SAPSec.Web/Controllers/TrackingController.cs
@@ -5,17 +5,20 @@ using Microsoft.AspNetCore.Mvc;
 namespace SAPSec.Web.Controllers;
 
 [AllowAnonymous]
-//[Route("tracking")]
+[Route("tracking")]
 public class TrackingController() : Controller
 {
-    // [DefaultBreadcrumb(PageTitles.ServiceHome)]
+    [HttpGet]
+    [Route("log-to-big-query")]
     public IActionResult LogToBigQuery(string? externalLink)
     {
-        //var startNowUrl = environment.IsProduction() ? configuration.Value.SignInUri : Url.Action("Index", "SchoolSearch", null);
+        if (externalLink is not null)
+        {
+            HttpContext.GetWebRequestEvent()?.AddData("External link click", externalLink);
 
-        HttpContext.GetWebRequestEvent()?.AddData("External link click", externalLink);
+            return Redirect(externalLink);
+        }
 
-
-        return Redirect(externalLink);
+        return NoContent();
     }
 }
