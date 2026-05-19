299d35f974315c896cab56a2ca8e8fcf04497aaa
Stu SHERWIN <Stu.SHERWIN@EDUCATION.GOV.UK>
2026-05-19 17:20:30 +0100

    Extract out Measure partial from Ks4CoreSubjects

---
+++ SAPSec.Web/ViewModels/MeasureViewModel.cs
@@ -0,0 +1,21 @@
+using SAPSec.Core.Model;
+using static SAPSec.Web.ViewModels.Ks4CoreSubjectsPageViewModel;
+
+namespace SAPSec.Web.ViewModels;
+
+public class MeasureViewModel
+{
+    public required string Key { get; set; }
+    public required string HtmlPrefix { get; set; }
+    public required string Title { get; set; }
+    public required SchoolDetails SchoolDetails { get; set; }
+    public required SubjectSection Subject { get; set; }
+    public required IEnumerable<GradeOptionViewModel> GradeOptions { get; set; }
+
+    public string SchoolLabel => SchoolDetails.Name;
+    public string SimilarSchoolsLabel => "Similar schools average";
+    public string LocalAuthorityLabel => "Local authority schools average";
+    public string EnglandLabel => "Schools in England average";
+}
+
+public record GradeOptionViewModel(string Key, string Name);
