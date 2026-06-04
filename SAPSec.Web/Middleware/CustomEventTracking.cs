2081e4e71145563f3728f4e2574c7dfdc8cf805f
Christopher Morriss <Christopher.MORRISS@EDUCATION.GOV.UK>
2026-06-04 17:11:37 +0100

    added inbound link clicks

--- SAPSec.Web/Middleware/CustomEventTracking.cs
+++ SAPSec.Web/Middleware/CustomEventTracking.cs
@@ -1,4 +1,6 @@
-﻿using Dfe.Analytics.Events;
+﻿using Dfe.Analytics.AspNetCore;
+using Dfe.Analytics.Events;
+using System.Text.RegularExpressions;
 
 namespace SAPSec.Web.Middleware
 {
@@ -6,17 +8,33 @@ namespace SAPSec.Web.Middleware
     {
         public static void MapCustomEventTracking(this IEndpointRouteBuilder app)
         {
-            app.MapPost("/custom-event-tracking", async (ClickData data, IEventSender eventSender) =>
+            app.MapPost("/custom-event-tracking", async (ClickData data, HttpContext httpContext, IEventSender eventSender) =>
             {
+                //Capture clicks on phase banner feedback link
                 if (data.Url.StartsWith("https://forms.cloud.microsoft", StringComparison.OrdinalIgnoreCase))
                 {
                     return await SendCustomEvent(eventSender, data, "feedback_link_click");
                 }
 
+                //Capture all external link clicks
                 if (!data.Url.StartsWith("https://get-school-improvement-insights-pr-240.test.teacherservices.cloud", StringComparison.OrdinalIgnoreCase))
                 {
                     return await SendCustomEvent(eventSender, data, "outbound_link_click");
                 }
+
+                //Capture inbound link clicks
+                //e.g. - /school/136994/ks4-headline-measures#year-by-year
+                //capture the text after the # symbol
+                Match match = Regex.Match(data.Url, @"(?<=#).*$");
+
+                if (match.Success)
+                {
+                    data.Text = match.Value;
+                    return await SendCustomEvent(eventSender, data, "inbound_link_click");
+                }
+
+                //If the data passed to /custom-event-tracking is not one of the above then don't record a web request event
+                httpContext.IgnoreWebRequestEvent();
 
                 return Results.NoContent();
             });
@@ -33,7 +51,11 @@ namespace SAPSec.Web.Middleware
         }
     }
 
-    public record ClickData(string Url, string Text);
+    public record ClickData
+    {
+        public string Text { get; set; }
+        public string Url { get; set; }
+    }
 
     //https://get-school-improvement-insights-pr-240.test.teacherservices.cloud
     //https://localhost:44300
