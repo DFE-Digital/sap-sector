0143e0fb0ae6c9b5627c67562cbeb88a6ce4ee29
mahmoodcli <101818885+mahmoodcli@users.noreply.github.com>
2026-06-19 14:44:00 +0100

    Fix user redirect to use phase-specific school routes (#255)
    ## Description
    
    Updates the user direct flow so users are sent to the correct school
    route for their current organisation.
    
    
    ## Related Trello Ticket
    
    https://trello.com/c/FwRDmpf8
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [x] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    
    ## How Has This Been Tested?
    
    Please describe the tests that you ran to verify your changes. Provide
    instructions so we can reproduce. Please also list any relevant details
    for your test configuration
    
    - [x] Unit tests added/updated 
    - [ ] Integration tests added/updated
    - [x] Manual testing performed
    - [ ] Tested on [Environment: Dev/Staging/Local]
    
    ## Checklist
    
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] I have verified accessibility requirements are met
    
    
    ## Screenshots (if applicable)
    
    N/A
    
    Co-authored-by: mahmoodcli <mahmoodcli@SL645718>

---
+++ Tests/SAPSec.Web.Tests/Controllers/UserControllerTests.cs
@@ -0,0 +1,88 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SAPSec.Core.Interfaces.Services;
+using SAPSec.Core.Model;
+using SAPSec.Web.Controllers;
+using System.Security.Claims;
+
+namespace SAPSec.Web.Tests.Controllers;
+
+public class UserControllerTests
+{
+    private readonly Mock<IUserService> _userService = new();
+    private readonly Mock<ILogger<UserController>> _logger = new();
+    private readonly UserController _controller;
+
+    public UserControllerTests()
+    {
+        _controller = new UserController(_userService.Object, _logger.Object);
+        _controller.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity(
+                    [new Claim(ClaimTypes.NameIdentifier, "user-123")],
+                    "TestAuth"))
+            }
+        };
+    }
+
+    [Fact]
+    public async Task Index_WhenCurrentOrganisationIsPrimary_RedirectsToPrimarySchoolRoute()
+    {
+        var user = new User
+        {
+            Sub = "user-123",
+            Organisations = [new Organisation { Id = "org-1", Urn = "123456" }]
+        };
+        var organisation = new Organisation
+        {
+            Id = "org-1",
+            Name = "Primary School",
+            Category = new Category { Name = "Establishment" },
+            Urn = "123456",
+            PhaseOfEducation = new PhaseOfEducation { Name = "Primary" }
+        };
+
+        _userService.Setup(x => x.GetUserFromClaimsAsync(It.IsAny<ClaimsPrincipal>()))
+            .ReturnsAsync(user);
+        _userService.Setup(x => x.GetCurrentOrganisationAsync(It.IsAny<ClaimsPrincipal>()))
+            .ReturnsAsync(organisation);
+
+        var result = await _controller.Index();
+
+        var redirect = result.Should().BeOfType<RedirectResult>().Subject;
+        redirect.Url.Should().Be("/school/primary/123456");
+    }
+
+    [Fact]
+    public async Task Index_WhenCurrentOrganisationIsSecondary_RedirectsToSecondarySchoolRoute()
+    {
+        var user = new User
+        {
+            Sub = "user-123",
+            Organisations = [new Organisation { Id = "org-2", Urn = "654321" }]
+        };
+        var organisation = new Organisation
+        {
+            Id = "org-2",
+            Name = "Secondary School",
+            Category = new Category { Name = "Establishment" },
+            Urn = "654321",
+            PhaseOfEducation = new PhaseOfEducation { Name = "Secondary" }
+        };
+
+        _userService.Setup(x => x.GetUserFromClaimsAsync(It.IsAny<ClaimsPrincipal>()))
+            .ReturnsAsync(user);
+        _userService.Setup(x => x.GetCurrentOrganisationAsync(It.IsAny<ClaimsPrincipal>()))
+            .ReturnsAsync(organisation);
+
+        var result = await _controller.Index();
+
+        var redirect = result.Should().BeOfType<RedirectResult>().Subject;
+        redirect.Url.Should().Be("/school/654321");
+    }
+}
