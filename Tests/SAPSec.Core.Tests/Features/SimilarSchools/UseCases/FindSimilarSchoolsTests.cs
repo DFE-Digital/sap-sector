45b742910f048421d613bfefdd7f042fcdc79874
Christopher Morriss <Christopher.MORRISS@EDUCATION.GOV.UK>
2026-06-26 10:59:48 +0100

    final governance structure UT updates

--- Tests/SAPSec.Core.Tests/Features/SimilarSchools/UseCases/FindSimilarSchoolsTests.cs
+++ Tests/SAPSec.Core.Tests/Features/SimilarSchools/UseCases/FindSimilarSchoolsTests.cs
@@ -1637,19 +1637,20 @@ public class FindSimilarSchoolsTests
             new() { URN = "100004", TrustSchoolFlagId = "3" },
             new() { URN = "100005", TrustSchoolFlagId = "1" },
             new() { URN = "100006", TrustSchoolFlagId = "2" },
-            new() { URN = "100007", TrustSchoolFlagId = "0", EstablishmentTypeGroupId = "4" }
+            new() { URN = "100007", TrustSchoolFlagId = "0" }
         );
         _similarSchoolsRepo.SetupGroups(
             new() { URN = "100001", NeighbourURN = "100002" },
             new() { URN = "100001", NeighbourURN = "100003" },
             new() { URN = "100001", NeighbourURN = "100004" },
             new() { URN = "100001", NeighbourURN = "100005" },
-            new() { URN = "100001", NeighbourURN = "100006" }
+            new() { URN = "100001", NeighbourURN = "100006" },
+            new() { URN = "100001", NeighbourURN = "100007" }
         );
 
         var response = await _sut.Execute(Request("100001", filterBy: new()
         {
-            ["np"] = ["Has Nursery Classes", "No Nursery Classes"]
+            ["gs"] = ["S", "M", "MS", "N"]
         }));
 
         response.FilterOptions.Should().SatisfyRespectively(
@@ -1657,15 +1658,154 @@ public class FindSimilarSchoolsTests
             f => f.Key.Should().Be("sciu"),
             f => f.Should().BeEquivalentTo(new
             {
-                Key = "np",
-                Name = "Nursery provision",
+                Key = "gs",
+                Name = "Governance structure",
                 Options = new[]
                 {
-                    new { Key = "Has Nursery Classes", Name = "Has Nursery Classes", Selected = true, Count = 2 },
-                    new { Key = "No Nursery Classes", Name = "No Nursery Classes", Selected = true, Count = 2 },
-                    new { Key = "Not applicable", Name = "Not applicable", Selected = false, Count = 1 }
+                    new { Key = "S", Name = "Single-academy trust (SAT)", Selected = true, Count = 1 },
+                    new { Key = "M", Name = "Multi-academy trust (MAT)", Selected = true, Count = 2 },
+                    new { Key = "MS", Name = "Maintained school - local authority controlled", Selected = true, Count = 2 },
+                    new { Key = "N", Name = "No known group", Selected = true, Count = 1 }
                 },
-                CurrentSchoolValue = DataWithAvailability.Available("Has Nursery Classes")
+                CurrentSchoolValue = DataWithAvailability.Available("Single-academy trust (SAT)")
+            }),
+            // Numeric range filters are always included
+            f => f.Key.Should().Be("oar"),
+            f => f.Key.Should().Be("par")
+        );
+    }
+
+    [Fact]
+    public async Task FilterBy_GovernanceStructure_LimitedToSimilarSchoolsGroup()
+    {
+        _establishmentRepo.SetupEstablishments(
+            new() { URN = "100001", TrustSchoolFlagId = "5" },
+            new() { URN = "100002", TrustSchoolFlagId = "5" },
+            new() { URN = "100003", TrustSchoolFlagId = "3" },
+            new() { URN = "100004", TrustSchoolFlagId = "3" },
+            new() { URN = "100005", TrustSchoolFlagId = "1" },
+            new() { URN = "100006", TrustSchoolFlagId = "2" },
+            new() { URN = "100007", TrustSchoolFlagId = "0" }
+        );
+        _similarSchoolsRepo.SetupGroups(
+            new() { URN = "100001", NeighbourURN = "100002" },
+            new() { URN = "100001", NeighbourURN = "100004" },
+            new() { URN = "100001", NeighbourURN = "100005" }
+        );
+
+        var response = await _sut.Execute(Request("100001", filterBy: new()
+        {
+            ["gs"] = ["S", "M", "MS", "N"]
+        }));
+
+        response.FilterOptions.Should().SatisfyRespectively(
+            // Numeric range filters are always included
+            f => f.Key.Should().Be("sciu"),
+            f => f.Should().BeEquivalentTo(new
+            {
+                Key = "gs",
+                Name = "Governance structure",
+                Options = new[]
+                {
+                    new { Key = "S", Name = "Single-academy trust (SAT)", Selected = true, Count = 1 },
+                    new { Key = "M", Name = "Multi-academy trust (MAT)", Selected = true, Count = 1},
+                    new { Key = "MS", Name = "Maintained school - local authority controlled", Selected = true, Count = 1 }
+                },
+                CurrentSchoolValue = DataWithAvailability.Available("Single-academy trust (SAT)")
+            }),
+            // Numeric range filters are always included
+            f => f.Key.Should().Be("oar"),
+            f => f.Key.Should().Be("par")
+        );
+    }
+
+    [Fact]
+    public async Task FilterBy_GovernanceStructure_FilterOptions_WhenOnlyOneFilterOptionAvailable_FilterIsExcluded()
+    {
+        _establishmentRepo.SetupEstablishments(
+            new() { URN = "100001" },
+            new() { URN = "100002", TrustSchoolFlagId = "5" },
+            new() { URN = "100003", TrustSchoolFlagId = "5" }
+
+        );
+        _similarSchoolsRepo.SetupGroups(
+            new() { URN = "100001", NeighbourURN = "100002" },
+            new() { URN = "100001", NeighbourURN = "100003" }
+        );
+
+        var response = await _sut.Execute(Request("100001"));
+ 
+        response.FilterOptions.Select(f => f.Key)
+            // Numeric range filters are always included
+            .Should().Equal("sciu", "oar", "par");
+    }
+
+    [Fact]
+    public async Task FilterBy_GovernanceStructure_FilterOptions_WhenOnlyOneFilterOptionAvailable_FilterIsIncluded()
+    {
+        _establishmentRepo.SetupEstablishments(
+            new() { URN = "100001" },
+            new() { URN = "100002", TrustSchoolFlagId = "5" },
+            new() { URN = "100003", TrustSchoolFlagId = "3" }
+        );
+        _similarSchoolsRepo.SetupGroups(
+            new() { URN = "100001", NeighbourURN = "100002" },
+            new() { URN = "100001", NeighbourURN = "100003" }
+        );
+
+        var response = await _sut.Execute(Request("100001"));
+
+        response.FilterOptions.Should().SatisfyRespectively(
+            // Numeric range filters are always included
+            f => f.Key.Should().Be("sciu"),
+            f => f.Should().BeEquivalentTo(new
+            {
+                Key = "gs",
+                Name = "Governance structure",
+                Options = new[]
+                {
+                    new { Key = "S", Name = "Single-academy trust (SAT)", Selected = false, Count = 1 },
+                    new { Key = "M", Name = "Multi-academy trust (MAT)", Selected = false, Count = 1 },
+                }
+            }),
+            // Numeric range filters are always included
+            f => f.Key.Should().Be("oar"),
+            f => f.Key.Should().Be("par")
+        );
+    }
+
+
+    [Theory]
+    [InlineData("5", "", "Single-academy trust (SAT)")]
+    [InlineData("3", "", "Multi-academy trust (MAT)")]
+    [InlineData("1", "", "Maintained school - local authority controlled")]
+    [InlineData("2", "", "Maintained school - local authority controlled")]
+    [InlineData("2", "4", "Maintained school - local authority controlled")]
+    [InlineData("0", "", "No known group")]
+    public async Task FilterBy_GovernanceStructure_FilterOptions_CurrentSchoolValue(string trustSchoolFlagId, string establishmentTypeGroupId, string? expectedCurrentSchoolValue)
+    {
+        _establishmentRepo.SetupEstablishments(
+            new() { URN = "100001", TrustSchoolFlagId = trustSchoolFlagId, EstablishmentTypeGroupId = establishmentTypeGroupId },
+            new() { URN = "100002", TrustSchoolFlagId = "5" },
+            new() { URN = "100003", TrustSchoolFlagId = "3" }
+        );
+        _similarSchoolsRepo.SetupGroups(
+            new() { URN = "100001", NeighbourURN = "100002" },
+            new() { URN = "100001", NeighbourURN = "100003" }
+        );
+
+        var response = await _sut.Execute(Request("100001"));
+
+        response.FilterOptions.Should().SatisfyRespectively(
+            // Numeric range filters are always included
+            f => f.Key.Should().Be("sciu"),
+            f => f.Should().BeEquivalentTo(new
+            {
+                Key = "gs",
+                Name = "Governance structure",
+                CurrentSchoolValue = expectedCurrentSchoolValue is null
+                    ? DataWithAvailability.NotAvailable<string>()
+                    : DataWithAvailability.Available(expectedCurrentSchoolValue)
             }),
             // Numeric range filters are always included
             f => f.Key.Should().Be("oar"),
