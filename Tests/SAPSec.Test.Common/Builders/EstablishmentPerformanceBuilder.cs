036d7f83c3d74e52c70356519a7443dfd940f2d5
Stu SHERWIN <Stu.SHERWIN@EDUCATION.GOV.UK>
2026-07-21 19:45:11 +0100

    Add tests

--- Tests/SAPSec.Test.Common/Builders/EstablishmentPerformanceBuilder.cs
+++ Tests/SAPSec.Test.Common/Builders/EstablishmentPerformanceBuilder.cs
@@ -7,12 +7,48 @@ public class EstablishmentPerformanceBuilder(string urn)
     string RwmExpected_Tot_Cohort_Est_Current_Num = string.Empty;
     string RwmExpected_Tot_Cohort_Est_Previous_Num = string.Empty;
     string RwmExpected_Tot_Cohort_Est_Previous2_Num = string.Empty;
+    string RwmExpected_Reading_Tot_Cohort_Est_Current_Num = string.Empty;
+    string RwmExpected_Reading_Tot_Cohort_Est_Previous_Num = string.Empty;
+    string RwmExpected_Reading_Tot_Cohort_Est_Previous2_Num = string.Empty;
+    string RwmExpected_Writing_Tot_Cohort_Est_Current_Num = string.Empty;
+    string RwmExpected_Writing_Tot_Cohort_Est_Previous_Num = string.Empty;
+    string RwmExpected_Writing_Tot_Cohort_Est_Previous2_Num = string.Empty;
+    string RwmExpected_Maths_Tot_Cohort_Est_Current_Num = string.Empty;
+    string RwmExpected_Maths_Tot_Cohort_Est_Previous_Num = string.Empty;
+    string RwmExpected_Maths_Tot_Cohort_Est_Previous2_Num = string.Empty;
 
     public EstablishmentPerformanceBuilder WithRwmExpected(string current, string prev, string prev2)
     {
         RwmExpected_Tot_Cohort_Est_Current_Num = current;
         RwmExpected_Tot_Cohort_Est_Previous_Num = prev;
         RwmExpected_Tot_Cohort_Est_Previous2_Num = prev2;
+
+        return this;
+    }
+
+    public EstablishmentPerformanceBuilder WithRwmExpectedReading(string current, string prev, string prev2)
+    {
+        RwmExpected_Reading_Tot_Cohort_Est_Current_Num = current;
+        RwmExpected_Reading_Tot_Cohort_Est_Previous_Num = prev;
+        RwmExpected_Reading_Tot_Cohort_Est_Previous2_Num = prev2;
+
+        return this;
+    }
+
+    public EstablishmentPerformanceBuilder WithRwmExpectedWriting(string current, string prev, string prev2)
+    {
+        RwmExpected_Writing_Tot_Cohort_Est_Current_Num = current;
+        RwmExpected_Writing_Tot_Cohort_Est_Previous_Num = prev;
+        RwmExpected_Writing_Tot_Cohort_Est_Previous2_Num = prev2;
+
+        return this;
+    }
+
+    public EstablishmentPerformanceBuilder WithRwmExpectedMaths(string current, string prev, string prev2)
+    {
+        RwmExpected_Maths_Tot_Cohort_Est_Current_Num = current;
+        RwmExpected_Maths_Tot_Cohort_Est_Previous_Num = prev;
+        RwmExpected_Maths_Tot_Cohort_Est_Previous2_Num = prev2;
 
         return this;
     }
@@ -24,5 +60,14 @@ public class EstablishmentPerformanceBuilder(string urn)
             RwmExpected_Tot_Cohort_Est_Current_Num = RwmExpected_Tot_Cohort_Est_Current_Num,
             RwmExpected_Tot_Cohort_Est_Previous_Num = RwmExpected_Tot_Cohort_Est_Previous_Num,
             RwmExpected_Tot_Cohort_Est_Previous2_Num = RwmExpected_Tot_Cohort_Est_Previous2_Num,
+            RwmExpected_Reading_Tot_Cohort_Est_Current_Num = RwmExpected_Reading_Tot_Cohort_Est_Current_Num,
+            RwmExpected_Reading_Tot_Cohort_Est_Previous_Num = RwmExpected_Reading_Tot_Cohort_Est_Previous_Num,
+            RwmExpected_Reading_Tot_Cohort_Est_Previous2_Num = RwmExpected_Reading_Tot_Cohort_Est_Previous2_Num,
+            RwmExpected_Writing_Tot_Cohort_Est_Current_Num = RwmExpected_Writing_Tot_Cohort_Est_Current_Num,
+            RwmExpected_Writing_Tot_Cohort_Est_Previous_Num = RwmExpected_Writing_Tot_Cohort_Est_Previous_Num,
+            RwmExpected_Writing_Tot_Cohort_Est_Previous2_Num = RwmExpected_Writing_Tot_Cohort_Est_Previous2_Num,
+            RwmExpected_Maths_Tot_Cohort_Est_Current_Num = RwmExpected_Maths_Tot_Cohort_Est_Current_Num,
+            RwmExpected_Maths_Tot_Cohort_Est_Previous_Num = RwmExpected_Maths_Tot_Cohort_Est_Previous_Num,
+            RwmExpected_Maths_Tot_Cohort_Est_Previous2_Num = RwmExpected_Maths_Tot_Cohort_Est_Previous2_Num,
         };
 }
