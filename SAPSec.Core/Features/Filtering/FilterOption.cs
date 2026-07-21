68c49090d71280f7fd2edb3583ae4f50c294c167
Stu SHERWIN <Stu.SHERWIN@EDUCATION.GOV.UK>
2026-07-21 12:06:44 +0100

    Add filtering

--- SAPSec.Core/Features/Filtering/FilterOption.cs
+++ SAPSec.Core/Features/Filtering/FilterOption.cs
@@ -3,5 +3,5 @@ namespace SAPSec.Core.Features.Filtering;
 public record FilterOption(
     string Key,
     string Name,
-    int Count,
-    bool Selected);
+    bool Selected,
+    int Count = 0);
