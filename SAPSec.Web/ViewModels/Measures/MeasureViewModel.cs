697309414a98b6fecb21e702a8f9c785a23377c9
Christopher Morriss <Christopher.MORRISS@EDUCATION.GOV.UK>
2026-07-21 15:30:44 +0100

    change to use overall absence instead of total absence

--- SAPSec.Web/ViewModels/Measures/MeasureViewModel.cs
+++ SAPSec.Web/ViewModels/Measures/MeasureViewModel.cs
@@ -28,7 +28,7 @@ public record MeasureViewModel(
         {
             Core.Constants.Measures.Primary.Ks2ExpectedRwm =>
                 "Meeting expected standard in reading, writing and maths",
-            Core.Constants.Measures.Primary.TotalAbsence =>
+            Core.Constants.Measures.Primary.OverallAbsence =>
                 "Total absence",
             _ => throw new InvalidOperationException($"No label found for Measure Key: {measureKey}")
         };
