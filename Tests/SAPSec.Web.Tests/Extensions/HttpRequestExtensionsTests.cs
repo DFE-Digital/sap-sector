3c6e6905a6168adac4a97d6205f45c886429290a
mahmoodcli <101818885+mahmoodcli@users.noreply.github.com>
2026-07-23 17:02:48 +0100

    Add dedicated /school/secondary route for secondary school journeys and area path (#282)
    ## Description
    
    This change introduces a dedicated secondary-school URL path under
    /school/secondary/{urn}.
    
    ## Related Trello Ticket
    
    https://trello.com/c/Ih9RRXAL
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] This change requires a documentation update
    
    ## How Has This Been Tested?
    
    Please describe the tests that you ran to verify your changes. Provide
    instructions so we can reproduce. Please also list any relevant details
    for your test configuration
    
    - [x] Unit tests added/updated 
    - [x] Integration tests added/updated
    - [ ] Manual testing performed
    - [x] Tested on [Environment: Dev/Staging/Local]
    
    ## Checklist
    
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] I have verified accessibility requirements are met
    
    
    ## Screenshots (if applicable)
    
    Add UI changes, logs, console outputs anything to help reviewers.
    
    ---------
    
    Co-authored-by: mahmoodcli <mahmoodcli@SL645718>

--- Tests/SAPSec.Web.Tests/Extensions/HttpRequestExtensionsTests.cs
+++ Tests/SAPSec.Web.Tests/Extensions/HttpRequestExtensionsTests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
+using SAPSec.Web.Constants;
 using SAPSec.Web.Extensions;
 
 namespace SAPSec.Web.Tests.Extensions;
@@ -9,27 +10,29 @@ public class HttpRequestExtensionsTests
     [Fact]
     public void GetCanonicalUrl_ReturnsSecondarySchoolUrlWithoutQueryString()
     {
+        var schoolDetailsUrl = Routes.SecondarySchool("123456").SchoolDetails;
         var request = new DefaultHttpContext().Request;
         request.Scheme = "https";
         request.Host = new HostString("service.education.gov.uk");
-        request.Path = "/school/123456/school-details";
+        request.Path = schoolDetailsUrl;
         request.QueryString = new QueryString("?sort=desc");
 
         var result = request.GetCanonicalUrl();
 
-        result.Should().Be("https://service.education.gov.uk/school/123456/school-details");
+        result.Should().Be($"https://service.education.gov.uk{schoolDetailsUrl}");
     }
 
     [Fact]
     public void GetCanonicalUrl_ReturnsPrimarySchoolUrl()
     {
+        var attendanceUrl = Routes.PrimarySchool("654321").Attendance;
         var request = new DefaultHttpContext().Request;
         request.Scheme = "https";
         request.Host = new HostString("service.education.gov.uk");
-        request.Path = "/school/primary/654321/attendance";
+        request.Path = attendanceUrl;
 
         var result = request.GetCanonicalUrl();
 
-        result.Should().Be("https://service.education.gov.uk/school/primary/654321/attendance");
+        result.Should().Be($"https://service.education.gov.uk{attendanceUrl}");
     }
 }
