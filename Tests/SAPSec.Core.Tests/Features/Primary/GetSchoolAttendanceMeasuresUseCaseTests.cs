4f11fb67c21030133a056791658bc8117cf0c883
Christopher Morriss <Christopher.MORRISS@EDUCATION.GOV.UK>
2026-07-29 07:49:38 +0100

    minor spacing update

--- Tests/SAPSec.Core.Tests/Features/Primary/GetSchoolAttendanceMeasuresUseCaseTests.cs
+++ Tests/SAPSec.Core.Tests/Features/Primary/GetSchoolAttendanceMeasuresUseCaseTests.cs
@@ -254,7 +254,7 @@ public class GetSchoolAttendanceMeasuresUseCaseTests
     [InlineData(Absence.Filters.Type.Values.Persistent, new[] { 2.27, 1.24, 8.20 }, new[] { 7.23, 7.29, 5.20 }, new[] { 3.20, 2.24, 2.20 })]
     //Empty or invalid filter values default to Overall absence
     [InlineData("", new[] { 5.20, 6.04, 4.30 }, new[] { 8.24, 5.44, 9.34 }, new[] { 3.24, 2.20, 1.20 })]
-    [InlineData("xyz", new[] { 5.20, 6.04, 4.30 }, new[] { 8.24, 5.44, 9.34 }, new[] { 3.24, 2.20, 1.20 } )]
+    [InlineData("xyz", new[] { 5.20, 6.04, 4.30 }, new[] { 8.24, 5.44, 9.34 }, new[] { 3.24, 2.20, 1.20 })]
     [Theory]
     public async Task Absence_FilterBy_Type_ContainsYearByYearValuesForSelectedType(string type, double[] currentSchool, double[] la, double[] england)
     {
