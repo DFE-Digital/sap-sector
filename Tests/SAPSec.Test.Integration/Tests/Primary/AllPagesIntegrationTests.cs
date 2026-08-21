c56288c7e97752465ae885e7aa121c67ba6ab155
mahmoodcli <mahmoodcli@SL645718>
2026-08-21 11:05:25 +0100

    Fixed unit tests

--- Tests/SAPSec.Test.Integration/Tests/Primary/AllPagesIntegrationTests.cs
+++ Tests/SAPSec.Test.Integration/Tests/Primary/AllPagesIntegrationTests.cs
@@ -15,7 +15,6 @@ public class AllPagesIntegrationTests(
     ITestOutputHelper outputHelper) : InMemoryRepositoryIntegrationTests(fixture, outputHelper)
 {
     private const string PrimarySchoolUrn = "100001";
-    private static readonly string ComparisonPath = Routes.PrimarySchool(PrimarySchoolUrn).Comparison("100002").Overview;
 
     private static readonly PageTestCase[] PrimaryPages = [
         new(Routes.PrimarySchool(PrimarySchoolUrn).Overview, "Test School 1", NavigationText: "Overview", IsOverviewPage: true),
@@ -23,8 +22,7 @@ public class AllPagesIntegrationTests(
         new(Routes.PrimarySchool(PrimarySchoolUrn).Attendance, "Attendance measures", NavigationText: "Attendance"),
         new(Routes.PrimarySchool(PrimarySchoolUrn).ViewSimilarSchools, "View similar schools"),
         new(Routes.PrimarySchool(PrimarySchoolUrn).SchoolDetails, "School details"),
-        new(Routes.PrimarySchool(PrimarySchoolUrn).WhatIsASimilarSchool, "What is a similar school?"),
-        new(ComparisonPath, "Test School 2", IsInNavigation: false)
+        new(Routes.PrimarySchool(PrimarySchoolUrn).WhatIsASimilarSchool, "What is a similar school?")
     ];
 
     public override Task InitializeAsync()
@@ -82,17 +80,7 @@ public class AllPagesIntegrationTests(
         var page = await Fixture.RequestPageAsync(path);
 
         var navigationItems = page.QuerySelectorAll(".govuk-breadcrumbs__list-item a");
-
-        if (path == ComparisonPath)
-        {
-            navigationItems.Should().SatisfyRespectively(
-                n => n.ShouldLinkTo("Home", Routes.FindASchool()),
-                n => n.ShouldLinkTo("View similar schools", Routes.PrimarySchool(PrimarySchoolUrn).ViewSimilarSchools));
-        }
-        else
-        {
-            navigationItems.Should().SatisfyRespectively(n => n.ShouldLinkTo("Home", Routes.FindASchool()));
-        }
+        navigationItems.Should().SatisfyRespectively(n => n.ShouldLinkTo("Home", Routes.FindASchool()));
     }
 
     [Theory]
