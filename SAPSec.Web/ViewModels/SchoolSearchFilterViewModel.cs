03fe9918f4a7aeaacd47df090cc8399b680d1a5d
Stu SHERWIN <Stu.SHERWIN@EDUCATION.GOV.UK>
2026-08-24 16:32:53 +0100

    Fix filter/pagination urls

--- SAPSec.Web/ViewModels/SchoolSearchFilterViewModel.cs
+++ SAPSec.Web/ViewModels/SchoolSearchFilterViewModel.cs
@@ -5,6 +5,4 @@ public class SchoolSearchFilterViewModel
     public string Query { get; set; } = string.Empty;
     public string[] LocalAuthorities { get; set; } = [];
     public string[]? SelectedLocalAuthorities { get; set; }
-    public bool SecondaryOnly { get; set; } = true;
-    public bool SimilarSchoolsOnly { get; set; } = true;
 }
