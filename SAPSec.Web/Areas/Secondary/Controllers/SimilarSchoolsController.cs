13bd5c0e7c5d603f87c0e57e945016db1d592dec
gayatri-dfe <gayatri.patel@eudcation.gov.uk>
2026-09-01 17:38:29 +0100

    RISE Resources - PR f/b

--- SAPSec.Web/Areas/Secondary/Controllers/SimilarSchoolsController.cs
+++ SAPSec.Web/Areas/Secondary/Controllers/SimilarSchoolsController.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SAPSec.Core.Constants;
 using SAPSec.Core.Features.SimilarSchools.UseCases;
+using SAPSec.Core.Interfaces.Services;
 using SAPSec.Web.Constants;
 using SAPSec.Web.Filters;
 using SAPSec.Web.Helpers;
 using SAPSec.Web.Services;
-using SAPSec.Core.Interfaces.Services;
 using SAPSec.Web.ViewModels;
 
 namespace SAPSec.Web.Areas.Secondary.Controllers;
@@ -18,15 +19,18 @@ public class SimilarSchoolsController : Controller
 {
     private readonly IRequestSchoolAccessor _requestSchoolAccessor;
     private readonly FindSimilarSchools _findSimilarSchools;
+    private readonly IFeatureFlagService _featureFlagService;
     private readonly ILogger<SimilarSchoolsController> _logger;
 
     public SimilarSchoolsController(
         IRequestSchoolAccessor requestSchoolAccessor,
         FindSimilarSchools findSimilarSchools,
+        IFeatureFlagService featureFlagService,
         ILogger<SimilarSchoolsController> logger)
     {
         _requestSchoolAccessor = requestSchoolAccessor;
         _findSimilarSchools = findSimilarSchools;
+        _featureFlagService = featureFlagService;
         _logger = logger;
     }
 
@@ -43,18 +47,11 @@ public class SimilarSchoolsController : Controller
         ViewData[ViewDataKeys.SchoolDetails] = school;
         if (Url is not null)
         {
-            var featureFlagService = HttpContext.RequestServices.GetService<IFeatureFlagService>();
-            var includeRise = false;
-            if (featureFlagService is not null)
-            {
-                includeRise = await featureFlagService.IsEnabledAsync(Core.Constants.FeatureFlags.EnableRiseResources);
-            }
-
             ViewData[ViewDataKeys.SchoolNavigation] = SchoolSideNavigationViewModel.CreateSecondary(
                 Url,
                 school?.Urn ?? urn,
                 nameof(ViewSimilarSchools),
-                includeRise);
+                await IsRiseResourcesEnabledAsync());
         }
 
         var filterBy = BuildCoreFilters(Request.Query);
@@ -149,4 +146,8 @@ public class SimilarSchoolsController : Controller
             ComparisonUrl = Routes.SecondarySchool(currentSchoolUrn).Comparison(school.URN).Similarity
         };
     }
+
+    private async Task<bool> IsRiseResourcesEnabledAsync() =>
+        _featureFlagService is not null
+        && await _featureFlagService.IsEnabledAsync(FeatureFlags.EnableRiseResources);
 }
