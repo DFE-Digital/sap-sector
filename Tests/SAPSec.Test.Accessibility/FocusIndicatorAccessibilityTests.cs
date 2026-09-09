40800694dd7dcb07ac59feeae1a889b1d5c34c2a
Stu SHERWIN <Stu.SHERWIN@EDUCATION.GOV.UK>
2026-09-09 19:46:20 +0100

    Fix tests

--- Tests/SAPSec.Test.Accessibility/FocusIndicatorAccessibilityTests.cs
+++ Tests/SAPSec.Test.Accessibility/FocusIndicatorAccessibilityTests.cs
@@ -84,7 +84,7 @@ public class FocusIndicatorAccessibilityTests(AccessibilityTestsFixture fixture)
         ");
 
         clusterLabels.Should().NotBeEmpty();
-        clusterLabels.Should().OnlyContain(label => label.Contains("containing"));
+        clusterLabels.Should().OnlyContain(label => label.Contains("including"));
     }
 
     private static async Task<string[]> GetFocusStyles(ILocator element)
