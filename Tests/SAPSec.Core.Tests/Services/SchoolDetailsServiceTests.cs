7aa95e79557426d585da8a1263d3e03ded8f3246
Stu SHERWIN <Stu.SHERWIN@EDUCATION.GOV.UK>
2026-09-09 10:58:35 +0100

    Fix tests

--- Tests/SAPSec.Core.Tests/Services/SchoolDetailsServiceTests.cs
+++ Tests/SAPSec.Core.Tests/Services/SchoolDetailsServiceTests.cs
@@ -182,7 +182,7 @@ public class SchoolDetailsServiceTests
         // Arrange
         _establishmentRepository
             .SetupEstablishments(Build.Establishment("123456", "Test Academy", x => x
-                .WithNurseryProvisionName(" ")));
+                .WithNurseryProvisionName("Has Nursery Classes")));
 
         // Act
         var result = await _sut.GetByUrnAsync("123456");
